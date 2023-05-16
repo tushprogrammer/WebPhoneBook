c98472dbb42769f052470243d99e0e04f59f603b
Dmitriy Pepelyaev <pepelyaev-94@mail.ru>
2023-05-16 21:46:36 +0500

    Добавьте файлы проекта.

---
+++ WebPhoneBook/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebPhoneBook
+{
+    public class Startup
+    {
+      
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSpaStaticFiles();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseMvc(
+                run =>
+                {
+                    run.MapRoute(
+                        name: "default",
+                        template: "{controller=Book}/{action=index}"
+                        );
+                });
+
+
+        }
+    }
+}
